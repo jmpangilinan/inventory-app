4f1eb4fbbb94701f3f2a54d8cfc34d9b80fe0e72
jmpangilinan <jmpangilinan@users.noreply.github.com>
2026-04-09 11:02:47 +0800

    fix: run e2e on chromium only in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,20 +12,27 @@ export default defineConfig({
     trace: "on-first-retry",
     screenshot: "only-on-failure",
   },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
+  projects: process.env.CI
+    ? [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+      ]
+    : [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+        {
+          name: "firefox",
+          use: { ...devices["Desktop Firefox"] },
+        },
+        {
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
+        },
+      ],
   webServer: {
     command: process.env.CI ? "bun start" : "bun dev",
     url: "http://localhost:3000",
